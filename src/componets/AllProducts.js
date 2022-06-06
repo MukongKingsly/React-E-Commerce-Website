56af9c0f967b51ea73f549e55961a31629b14c27
Kingsly Mbaaka <kingmbamu@yahoo.co.uk>
2022-06-06 10:27:56 +0200

    updated packages

---
+++ src/componets/AllProducts.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+function AllProducts() {
+  return (
+    <div>AllProducts</div>
+  )
+}
+
+export default AllProducts
--- src/componets/FeaturedCategories.js
+++ src/componets/FeaturedCategories.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import { Category } from '../images/ProjectImg';
+import { Category, LatestProductsImg  } from '../images/ProjectImg';
 import './featured.css';
 
 function FeaturedCategories() {
+    let number = Math.floor(Math.random()*LatestProductsImg.length);
+    let myArray = [];
+    for (let i=0; i++; i<LatestProductsImg.length) {
+        myArray.push(i)
+    }
+
   return (
     <div className="categories">
     <div className="small-container">
@@ -14,7 +20,7 @@ function FeaturedCategories() {
                     </div>)
             })}
         </div>
-    </div>        
+    </div>
 </div>
   )
 }
